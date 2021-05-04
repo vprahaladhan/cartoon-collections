9ea740756c5d26274ed2d0ff4d15b3b92e51e16c
prahal <vprahaladhan@gmail.com>
2021-05-04 20:36:51 +1000

    commit tree a8d3503d540289d9bb6e66a6490c353e98019610

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index| 
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements) # code an argument here
   # Your code here
+  elements.map! do |element|
+    element.capitalize() << "!"
+  end     
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.any? {|call| call.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(elements)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  idx = nil
+
+  elements.each_with_index do |element, index| 
+    if cheese_types.include?(element) then idx = index
+    end
+  end
+
+  idx ? elements[idx] : idx
 end
